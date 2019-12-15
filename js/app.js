e188bc0cdabd6b9146b53100f5ecb30aeab8c0d3
Arno <arno@delandtsheer.eu>
2019-12-15 20:22:58 +0100

    added timeline

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ var ENDPOINTAll = 'https://api.spacexdata.com/v3/launches?sort=launch_date_utc';
 let position = 0,
 	positionstars = 0,
 	amount,
-	index;
+	index_next,
+	date_last;
 
 const FULL_CIRCLE = 2 * Math.PI;
 let ctx, timer, particles;
@@ -39,7 +40,6 @@ const loadCanvas = (width = 15) => {
 		drawStars(ctx);
 	});
 };
-
 
 const getData = async function() {
 	try {
@@ -94,17 +94,19 @@ const getRocket = (rocket_n, rocket_t, rocket_f, rocket_b) => {
 		default:
 			return '<img class="c-rocketimg-out" src="img/F9_v1.2_Fi_Pa.svg"></img>';
 	}
-}
+};
 
 const showTimer = function(time, i) {
 	var interval = setInterval(function() {
-		var now = new Date().getTime();
-		var distance = time - now;
-
-		var hours = Math.floor(distance / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+		var now = new Date(),
+			distance = time - now.getTime(),
+			distance_passed = now - date_last,
+			distance_tot = time - date_last,
+			hours = Math.floor(distance / (1000 * 60 * 60)),
+			minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+			seconds = Math.floor((distance % (1000 * 60)) / 1000),
+			passed = Math.round(((distance_passed) / distance_tot) * 100);
+		document.querySelector(`.js-timeline${i}`).style.width = `${passed}%`;
 		document.querySelector(`.js-countdown${i}`).innerHTML = hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds ';
 	}, 1000);
 };
@@ -113,24 +115,29 @@ const showData = function(data) {
 	var i = 0;
 
 	data.forEach(element => {
-		let reusedtext, rocketimg, datetime = new Date(element.launch_date_utc);
+		var reusedtext,
+			rocketimg,
+			progression,
+			datetime = new Date(element.launch_date_utc),
+			location = element.launch_site.site_name_long,
+			rocket_n = element.rocket.rocket_name,
+			rocket_t = element.rocket.rocket_type,
+			rocket_f = element.rocket.first_stage.cores[0].gridfins,
+			rocket_b = element.rocket.first_stage.cores[0].block,
+			orbit = element.rocket.second_stage.payloads[0].orbit_params.reference_system,
+			position = element.rocket.second_stage.payloads[0].orbit_params.regime,
+			reused = element.rocket.second_stage.payloads[0].reused,
+			mission = element.mission_name,
+			link = element.links.video_link,
+			missionnr = element.flight_number,
+			details = element.details,
+			payload = element.rocket.second_stage.payloads.payload_id,
+			client = element.rocket.second_stage.payloads.customers,
+			nationality = element.rocket.second_stage.payloads[0].nationality,
+			manufacturer = element.rocket.second_stage.payloads[0].manufacturer,
+			payload_type = element.rocket.second_stage.payloads[0].payload_type;
 
 		if (datetime < new Date().getTime()) {
-			var location = element.launch_site.site_name_long;
-			var rocket_n = element.rocket.rocket_name;
-			var rocket_t = element.rocket.rocket_type;
-			var rocket_f = element.rocket.first_stage.cores[0].gridfins;
-			var rocket_b = element.rocket.first_stage.cores[0].block;
-			var orbit = element.rocket.second_stage.payloads[0].orbit_params.reference_system;
-			var position = element.rocket.second_stage.payloads[0].orbit_params.regime;
-			var reused = element.rocket.second_stage.payloads[0].reused;
-			var mission = element.mission_name;
-			var link = element.links.video_link;
-			var missionnr = element.flight_number;
-			var details = element.details;
-			var payload = element.rocket.second_stage.payloads.payload_id;
-			var client = element.rocket.second_stage.payloads.customers;
-
 			switch (missionnr) {
 				case 1:
 					missionnr = '1st';
@@ -157,8 +164,8 @@ const showData = function(data) {
 				link = '';
 			}
 
-			rocketimg = getRocket(rocket_n, rocket_t, rocket_f, rocket_b)
-
+			rocketimg = getRocket(rocket_n, rocket_t, rocket_f, rocket_b);
+			date_last = datetime;
 
 			var HTML = `
 			<div class="c-content">
@@ -168,26 +175,13 @@ const showData = function(data) {
 				<p class="c-title">The ${missionnr} mission was ${mission}</p>
 				<p class="c-details">"${details}"</p>
 				<div class="c-textarea">
-				<p class="c-title">The payload was ${payload} in a ${reusedtext} ${rocket_n} for ${client}</p>
-				<p class="c-title">This mission launched from ${location}</p>
+				<p>The payload was ${payload} in a ${reusedtext} ${rocket_n} for ${client}</p>
+				<p>This mission launched from ${location}</p>
 				</div>
 			</div>`;
 
 			document.querySelector('.c-container').innerHTML += HTML;
-		} 
-		else 
-		{
-			var location = element.launch_site.site_name_long;
-			var rocket_n = element.rocket.rocket_name;
-			var rocket_t = element.rocket.rocket_type;
-			var rocket_f = element.rocket.first_stage.cores[0].gridfins;
-			var rocket_b = element.rocket.first_stage.cores[0].block;
-			var orbit = element.rocket.second_stage.payloads[0].orbit_params.reference_system;
-			var position = element.rocket.second_stage.payloads[0].orbit_params.regime;
-			var reused = element.rocket.second_stage.payloads[0].reused;
-			var mission = element.mission_name;
-			var link = element.links.video_link;
-
+		} else {
 			if (reused) {
 				reusedtext = 'reused';
 			} else {
@@ -200,34 +194,43 @@ const showData = function(data) {
 				link = '';
 			}
 
-			rocketimg = getRocket(rocket_n, rocket_t, rocket_f, rocket_b)
+			if (index_next == null) {
+				index_next = i;
+			} else {
+			}
 
+			progression = rocketimg = getRocket(rocket_n, rocket_t, rocket_f, rocket_b);
 
 			var HTML = `
 			<div class="c-content">
 				<img class="c-logo" src="img/logo-white.svg"></img>
 				${rocketimg}
 				<div class="c-form">
-				<p class="c-title">The next mission is ${mission} and launches in</p>
-				<p class="c-countdown"><span class="js-countdown${i}"></span></p>
-				<span class="c-timepassed" style="background-color:#1a1763; grid-row: 4; grid-column: 2; width: 40%;"></span><span style="background-color:#A7A9AC66;grid-row: 4; grid-column: 2;"></span>
-				<p class="c-rocketkind">In a ${reusedtext} ${rocket_n}</p>
-				<div class="c-textarea">
-					<p class="c-orbit">Going into a ${orbit} ${position} orbit</p>
-					<p class="c-date">On ${datetime.toLocaleDateString()} at ${datetime.toLocaleTimeString()} in your local timezone</p>
-					<p class="c-launchloc">At ${location}</p>
-					${link}
-				</div>
+					<div title="Timer may deviate if launch date is not confirmed yet and placed on the 1st of the planned month.">
+						<p class="c-title">Upcomming mission ${missionnr} is ${mission} and launches in</p>
+					</div>
+					<p class="c-countdown js-countdown${i}"></p>
+					<div class="c-timeline">
+						<p class="c-timeline_text_past">Last launch</p>
+						<p class="c-timeline_text_next">This launch</p>
+					</div>
+					<div class="c-timeline-bar">
+						<div class="c-timeline_passed js-timeline${i}"></div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="transform: translate(-12px, -7px) scale(1.5);"><path d="M0 0h24v24H0z" fill="none"/><path fill="SLATEGRAY" d="M7.41 18.59L8.83 20 12 16.83 15.17 20l1.41-1.41L12 14l-4.59 4.59zm9.18-13.18L15.17 4 12 7.17 8.83 4 7.41 5.41 12 10l4.59-4.59z"/></svg>
+					</div>
+					<div class="c-textarea">
+						<p class="c-rocketkind">In a ${reusedtext} ${rocket_n}</p>
+						<p class="c-orbit">Going into a ${orbit} ${position} orbit</p>
+						<p class="c-orbit">The payload is a ${payload_type} made by ${manufacturer} in ${nationality}</p>
+						<p class="c-date">Launchdate is ${datetime.toLocaleDateString('nl-BE')} at ${datetime.toLocaleTimeString('nl-BE')} in your local timezone</p>
+						<p class="c-launchloc">At ${location}</p>
+						${link}
+					</div>
 				</div>
 			</div>`;
 
 			document.querySelector('.c-container').innerHTML += HTML;
 			showTimer(datetime, i);
-
-
-			if (index == null) {
-				index = i;
-			}
 		}
 		i++;
 		amount++;
@@ -239,22 +242,32 @@ const showData = function(data) {
 	});
 	document.querySelector('.c-loading').style.display = 'none';
 
-	position = index * 100 * -1;
+	position = index_next * 100 * -1;
 	document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
 };
 
-
+/* aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa fix da knop hide als het moet */
 
 const init = function() {
 	document.querySelector('.c-button-right').addEventListener('click', function() {
 		position -= 100;
 		positionstars -= 32;
+		if (index_next >= amount) {
+			document.querySelector('.c-button-right').style.display = 'none';
+		} else {
+			document.querySelector('.c-button-right').style.display = 'block';
+		}
 		document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
 		document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
 	});
 	document.querySelector('.c-button-left').addEventListener('click', function() {
 		position += 100;
 		positionstars += 32;
+		if (index_next <= amount) {
+			document.querySelector('.c-button-left').style.display = 'none';
+		} else {
+			document.querySelector('.c-button-left').style.display = 'block';
+		}
 		document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
 		document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
 	});
