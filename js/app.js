a809a1407c52dae5f6057eb7691bb1f567b8e51e
Arno <arno@delandtsheer.eu>
2019-12-18 23:47:39 +0100

    mobile fixings, landing page

--- js/app.js
+++ js/app.js
@@ -41,6 +41,60 @@ const loadCanvas = (width = 15) => {
 	});
 };
 
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+	return (
+		evt.touches || evt.originalEvent.touches // browser API
+	); // jQuery
+}
+
+function handleTouchStart(evt) {
+	const firstTouch = getTouches(evt)[0];
+	xDown = firstTouch.clientX;
+	yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+	if (!xDown || !yDown) {
+		return;
+	}
+
+	var xUp = evt.touches[0].clientX;
+	var yUp = evt.touches[0].clientY;
+
+	var xDiff = xDown - xUp;
+	var yDiff = yDown - yUp;
+
+	if (Math.abs(xDiff) > Math.abs(yDiff)) {
+		/*most significant*/
+		if (xDiff > 0) {
+			position -= 100;
+			positionstars -= 32;
+			document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
+			document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
+		} else {
+			position += 100;
+			positionstars += 32;
+			document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
+			document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
+		}
+	} else {
+		if (yDiff > 0) {
+			/* up swipe */
+		} else {
+			/* down swipe */
+		}
+	}
+	/* reset values */
+	xDown = null;
+	yDown = null;
+}
+
 const getData = async function() {
 	try {
 		const data = await fetch(ENDPOINTAll)
@@ -200,8 +254,8 @@ const showData = function(data) {
 						<p>This mission launched from ${location}</p>
 					</div>
 					<div style="display:flex;">
-						<a href="${imglink1}" class="c-img_left"><img src="${imglink1}"></img></a>
-						<a href="${imglink2}" class="c-img_right"><img src="${imglink2}"></img></a>
+						<a href="${imglink1}" class="c-img_left c-img"><img src="${imglink1}"></img></a>
+						<a href="${imglink2}" class="c-img_right c-img"><img src="${imglink2}"></img></a>
 					</div>
 				</div>
 			</div>`;
@@ -243,7 +297,7 @@ const showData = function(data) {
 						</div>
 						<div class="c-timeline-bar">
 							<div class="c-timeline_passed js-timeline${i}"></div>
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="transform: translate(-12px, -7px) scale(1.5);"><path d="M0 0h24v24H0z" fill="none"/><path fill="SLATEGRAY" d="M7.41 18.59L8.83 20 12 16.83 15.17 20l1.41-1.41L12 14l-4.59 4.59zm9.18-13.18L15.17 4 12 7.17 8.83 4 7.41 5.41 12 10l4.59-4.59z"/></svg>
+							<svg class="c-timeline-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="SLATEGRAY" d="M7.41 18.59L8.83 20 12 16.83 15.17 20l1.41-1.41L12 14l-4.59 4.59zm9.18-13.18L15.17 4 12 7.17 8.83 4 7.41 5.41 12 10l4.59-4.59z"/></svg>
 						</div>
 					</div>
 					<div class="c-textarea">
@@ -301,9 +355,9 @@ const init = function() {
 	});
 
 	let data = sessionStorage.getItem('data');
-	if (data == null){
-		getData()
-	}else{
+	if (data == null) {
+		getData();
+	} else {
 		showData(JSON.parse(data));
 	}
 };
