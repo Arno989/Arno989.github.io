749c3bd5905b51361e835482d1d509a0d2b57bbe
Arno Defauw <arno@delandtsheer.eu>
2021-03-04 11:58:46 +0100

    project cleanup

--- js/app.js
+++ js/app.js
@@ -1,44 +1,62 @@
 /* 
 Known bugs:
-Stars arent wide enough
+Stars arent wide enough  ----------  at css fix container and star container at same time sithout display flex
 Bigg lag
 mobilen't
 
 */
-var ENDPOINT = 'https://api.spacexdata.com/v4/launches/';
-var ENDPOINTAll = 'https://api.spacexdata.com/v4/launches?sort=launch_date_utc';
+var ENDPOINT = 'https://api.spacexdata.com/v3/launches/';
+var ENDPOINTAll = 'https://api.spacexdata.com/v3/launches?sort=launch_date_utc';
 let position = 0,
 	positionstars = 0,
-	amount,
+	amount = 0,
 	index_next,
 	date_last;
 
-const FULL_CIRCLE = 2 * Math.PI;
-let ctx, timer, particles;
+let ctx, timer, particles, button_right, button_left;
 
-const drawStars = ctx => {
+const drawStars = (ctx) => {
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	for (let i = 0; i < 20000; i++) {
+	for (let i = 0; i < 1300; i++) {
 		ctx.beginPath();
 		let x = Math.random() * ctx.canvas.width,
 			y = Math.random() * ctx.canvas.height,
 			size = Math.random() * 2;
 
-		ctx.arc(x, y, size, 0, FULL_CIRCLE);
+		ctx.arc(x, y, size, 0, 2 * Math.PI);
 		ctx.fillStyle = `rgba(255, 241, 194, ${Math.random()})`;
-		/* ctx.fillStyle = 'rgb(255, 241, 194)'; */
 		ctx.fill();
 	}
 };
 
-const loadCanvas = (width = 15) => {
-	const canvas = document.querySelectorAll('.js-stars');
-	canvas.forEach(element => {
-		element.width = document.documentElement.clientWidth * width;
-		element.height = document.documentElement.clientHeight;
-		ctx = element.getContext('2d');
-		drawStars(ctx);
-	});
+const loadCanvas = (amount) => {
+	// make 1 screen pattern of stars for process efficiency
+	const patternCanvas = document.querySelector('.js-starpattern');
+	const patternContext = patternCanvas.getContext('2d');
+
+	patternCanvas.width = document.documentElement.clientWidth;
+	patternCanvas.height = document.documentElement.clientHeight;
+
+	drawStars(patternContext);
+	dataUrl = patternCanvas.toDataURL();
+
+	/*
+	const canvas = document.querySelector('.js-starcanvas');
+	canvas.width = document.documentElement.clientWidth * amount;
+	canvas.height = document.documentElement.clientHeight;
+
+	ctx = canvas.getContext('2d');
+	pattern = ctx.createPattern(patternCanvas, 'repeat');
+	ctx.fillStyle = pattern;
+
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	canvas.create;
+	*/
+
+	container_stars = document.querySelector('.c-container-stars');
+	container_stars.style.width = `${document.documentElement.clientWidth * (amount / 2.8)}px`;
+	container_stars.style.background = 'url(' + dataUrl + ')';
 };
 
 document.addEventListener('touchstart', handleTouchStart, false);
@@ -76,12 +94,12 @@ function handleTouchMove(evt) {
 			position -= 100;
 			positionstars -= 32;
 			document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
-			document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
+			document.querySelector('.js-starpattern').style.transform = `translateX(${positionstars}vw)`;
 		} else {
 			position += 100;
 			positionstars += 32;
 			document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
-			document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
+			document.querySelector('.js-starpattern').style.transform = `translateX(${positionstars}vw)`;
 		}
 	} else {
 		if (yDiff > 0) {
@@ -95,11 +113,11 @@ function handleTouchMove(evt) {
 	yDown = null;
 }
 
-const getData = async function() {
+const getData = async function () {
 	try {
 		const data = await fetch(ENDPOINTAll)
-			.then(r => r.json())
-			.then(data => data);
+			.then((r) => r.json())
+			.then((data) => data);
 
 		var json = JSON.stringify(data);
 		sessionStorage.setItem('data', json);
@@ -153,8 +171,8 @@ const getRocket = (rocket_n, rocket_t, rocket_f, rocket_b) => {
 	}
 };
 
-const showTimer = function(time, i) {
-	var interval = setInterval(function() {
+const showTimer = function (time, i) {
+	var interval = setInterval(function () {
 		var now = new Date(),
 			distance = time - now.getTime(),
 			distance_passed = now - date_last,
@@ -167,8 +185,14 @@ const showTimer = function(time, i) {
 			passed = Math.round((distance_passed / distance_tot) * 100);
 		document.querySelector(`.js-timeline${i}`).style.width = `${passed}%`;
 
-		if (hours > 48) {
-			document.querySelector(`.js-countdown${i}`).innerHTML = days + ' days, ' + dhours + ' hours and </br>' + minutes + ' minutes';
+		if (days > 5) {
+			document.querySelector(`.js-countdown${i}`).innerHTML = days + ' days';
+		} else if (hours > 48) {
+			if (minutes != 0) {
+				document.querySelector(`.js-countdown${i}`).innerHTML = days + ' days, ' + dhours + ' hours and </br>' + minutes + ' minutes';
+			} else {
+				document.querySelector(`.js-countdown${i}`).innerHTML = days + ' days, ' + dhours + ' hours';
+			}
 		} else {
 			if (seconds != 0) {
 				document.querySelector(`.js-countdown${i}`).innerHTML = hours + ' hours, ' + minutes + ' minutes and </br>' + seconds + ' seconds ';
@@ -179,10 +203,18 @@ const showTimer = function(time, i) {
 	}, 1000);
 };
 
-const showData = function(data) {
+function truncate(str, n, useWordBoundary) {
+	if (str.length <= n) {
+		return str;
+	}
+	const subString = str.substr(0, n - 1); // the original check
+	return (useWordBoundary ? subString.substr(0, subString.lastIndexOf(' ')) : subString) + ' &hellip;';
+}
+
+const showData = function (data) {
 	var i = 0;
 
-	data.forEach(element => {
+	data.forEach((element) => {
 		var reusedtext,
 			rocketimg,
 			link,
@@ -200,8 +232,8 @@ const showData = function(data) {
 			imglink = element.links.flickr_images,
 			missionnr = element.flight_number,
 			details = element.details,
-			payload = element.rocket.second_stage.payloads.payload_id,
-			client = element.rocket.second_stage.payloads.customers,
+			payload = element.rocket.second_stage.payloads[0].payload_id,
+			client = element.rocket.second_stage.payloads[0].customers[0],
 			nationality = element.rocket.second_stage.payloads[0].nationality,
 			manufacturer = element.rocket.second_stage.payloads[0].manufacturer,
 			payload_type = element.rocket.second_stage.payloads[0].payload_type;
@@ -236,27 +268,35 @@ const showData = function(data) {
 			rocketimg = getRocket(rocket_n, rocket_t, rocket_f, rocket_b);
 			date_last = datetime;
 
-			imglink1 = imglink[Math.floor(Math.random() * imglink.length)];
-			imglink2 = imglink[Math.floor(Math.random() * imglink.length)];
-			if (imglink1 == imglink2) {
-				imglink2 = imglink[Math.floor(Math.random() * imglink.length)];
-			}
-
 			var HTML = `
 			<div class="c-content">
 				<img class="c-logo" src="img/logo-white.svg"></img>
 				${rocketimg}
 				<div class="c-form">
-					<p class="c-title">The ${missionnr} mission was ${mission}</p>
-					<p class="c-details">"${details}"</p>
+					<p class="c-title">The ${missionnr} mission was ${mission}</p>`;
+
+			if (details != null) {
+				HTML += `
+				<p class="c-details" title="${details}">"${truncate(details, 250)}"</p>
+				`;
+			}
+
+			HTML += `
 					<div class="c-textarea">
 						<p>The payload was ${payload} in a ${reusedtext} ${rocket_n} for ${client}</p>
 						<p>This mission launched from ${location}</p>
-					</div>
+					</div>`;
+
+			var imglink1, imglink2;
+			if (imglink.length != 0) {
+				imglink1 = imglink[Math.floor(Math.random() * imglink.length)];
+				HTML += `
 					<div style="display:flex;">
-						<a href="${imglink1}" class="c-img_left c-img"><img src="${imglink1}"></img></a>
-						<a href="${imglink2}" class="c-img_right c-img"><img src="${imglink2}"></img></a>
-					</div>
+						<a href="${imglink1}" class="c-img_center c-img"><img src="${imglink1}"></img></a>
+					</div>`;
+			}
+
+			HTML += `
 				</div>
 			</div>`;
 
@@ -317,42 +357,57 @@ const showData = function(data) {
 		i++;
 		amount++;
 	});
+	if (index_next == null) {
+		index_next = i - 4;
+	}
 
-	loadCanvas();
-	document.querySelectorAll('.c-hidden').forEach(element => {
+	loadCanvas(amount);
+	document.querySelectorAll('.c-hidden').forEach((element) => {
 		element.classList.remove('c-hidden');
 	});
 	document.querySelector('.c-loading').style.display = 'none';
 
 	position = index_next * 100 * -1;
+	positionstars = index_next * 33 * -1;
 	document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
+	document.querySelector('.c-container-stars').style.transform = `translateX(${positionstars}vw)`;
 };
 
-/* aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa fix da knop hide als het moet */
+const hideButtons = () => {
+	if ((position / 100) * -1 < amount - 1 && (position / 100) * -1 > 0) {
+		button_right.style.display = 'block';
+		button_left.style.display = 'block';
+	} else if ((position / 100) * -1 >= amount - 1) {
+		button_right.style.display = 'none';
+	} else if ((position / 100) * -1 <= 0) {
+		button_left.style.display = 'none';
+	}
+};
 
-const init = function() {
-	document.querySelector('.c-button-right').addEventListener('click', function() {
+const init = function () {
+	button_right = document.querySelector('.c-button-right');
+	button_left = document.querySelector('.c-button-left');
+
+	button_right.addEventListener('click', function () {
 		position -= 100;
-		positionstars -= 32;
-		if (index_next >= amount) {
-			document.querySelector('.c-button-right').style.display = 'none';
-		} else {
-			document.querySelector('.c-button-right').style.display = 'block';
-		}
+		positionstars -= 33;
+		hideButtons();
+
 		document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
-		document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
+		document.querySelector('.c-container-stars').style.transform = `translateX(${positionstars}vw)`;
 	});
-	document.querySelector('.c-button-left').addEventListener('click', function() {
+	button_left.addEventListener('click', function () {
 		position += 100;
-		positionstars += 32;
-		if (index_next <= amount) {
-			document.querySelector('.c-button-left').style.display = 'none';
-		} else {
-			document.querySelector('.c-button-left').style.display = 'block';
-		}
+		positionstars += 33;
+		hideButtons();
+
 		document.querySelector('.c-container').style.transform = `translateX(${position}vw)`;
-		document.querySelector('.js-stars').style.transform = `translateX(${positionstars}vw)`;
+		document.querySelector('.c-container-stars').style.transform = `translateX(${positionstars}vw)`;
 	});
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	init();
 
 	let data = sessionStorage.getItem('data');
 	if (data == null) {
@@ -360,8 +415,4 @@ const init = function() {
 	} else {
 		showData(JSON.parse(data));
 	}
-};
-
-document.addEventListener('DOMContentLoaded', function() {
-	init();
 });
